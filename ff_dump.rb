f87f49f842479ce7cea0e5755ab3654c3ccc6e9a
NAKAMURA <nahi@pekoe.(none)>
2009-06-20 17:55:41 +0900

    broken UTF-8 encoding support.

--- ff_dump.rb
+++ ff_dump.rb
@@ -9,6 +9,42 @@ def usage
   "#{$0} nickname remote_api_key dump_dir"
 end
 
+module JSONFilter
+  class << self
+    def parse(str)
+      safe = filter_utf8(str)
+      JSON.parse(safe)
+    end
+
+    def pretty_generate(obj)
+      JSON.pretty_generate(obj)
+    end
+
+  private
+
+    # these definition source code is from soap4r.
+    us_ascii = '[\x9\xa\xd\x20-\x7F]'     # XML 1.0 restricted.
+    # 0xxxxxxx
+    # 110yyyyy 10xxxxxx
+    twobytes_utf8 = '(?:[\xC0-\xDF][\x80-\xBF])'
+    # 1110zzzz 10yyyyyy 10xxxxxx
+    threebytes_utf8 = '(?:[\xE0-\xEF][\x80-\xBF][\x80-\xBF])'
+    # 11110uuu 10uuuzzz 10yyyyyy 10xxxxxx
+    fourbytes_utf8 = '(?:[\xF0-\xF7][\x80-\xBF][\x80-\xBF][\x80-\xBF])'
+    CHAR_UTF_8 = "(?:#{us_ascii}|#{twobytes_utf8}|#{threebytes_utf8}|#{fourbytes_utf8})"
+
+    def filter_utf8(str)
+      str.scan(/(#{CHAR_UTF_8})|(.)/n).collect { |u, x|
+        if u
+          u
+        else
+          sprintf("\\x%02X", x[0])
+        end
+      }.join
+    end
+  end
+end
+
 USER = ARGV.shift or raise usage
 REMOTE_KEY = ARGV.shift or raise usage
 DUMP_DIR = ARGV.shift or raise usage
@@ -28,7 +64,7 @@ end
 client.set_auth(nil, USER, REMOTE_KEY)
 
 URLS.each do |url|
-  JSON.parse(client.get_content(url))['entries'].each do |entry|
+  JSONFilter.parse(client.get_content(url))['entries'].each do |entry|
     published = entry['published']
     id = entry['id']
     filename = File.join(DUMP_DIR, "#{published}_#{id}.xml")
